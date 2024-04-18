bf09d48f13d30646ae34cf52e04e61d011f288b9
baeksu <kyungwoo5826@naver.com>
2024-04-18 23:53:18 +0900

    try to add custom header

--- api-server/index.js
+++ api-server/index.js
@@ -3,6 +3,10 @@ const fs = require("fs");
 //config file 경로
 const filePath = "../config.json";
 
+//app key & app secret 세팅
+let appKey = "";
+let appSecret = "";
+
 fs.readFile(filePath, "utf-8", (err, data) => {
     if (err) {
         console.error("파일을 읽는 도중 오류가 발생했습니다!", err);
@@ -10,5 +14,20 @@ fs.readFile(filePath, "utf-8", (err, data) => {
     }
 
     const configData = JSON.parse(data);
-    console.log(configData);
+    appkey = configData.APPKEY;
+    appSecret = configData.APPSECRET;
 });
+
+//접근토큰발급을 위한 request header
+const accessAuthorizationRequestHeader = {
+    grant_type: "client_credentials",
+    appkey: appKey,
+    appsecret: appSecret,
+};
+
+//request header
+const financialRatioRequestHeader = {
+    "content-type": "application/json",
+    appkey: appKey,
+    appSecret: appSecret,
+};
